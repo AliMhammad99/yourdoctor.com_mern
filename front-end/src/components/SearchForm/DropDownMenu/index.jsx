397fe0c8f03afeb4aa2e75a99a75a94dceaa1fad
AliMhammad99 <alimhmd.engineer@gmail.com>
2022-04-21 12:31:02 +0300

    In Progress

--- front-end/src/components/SearchForm/DropDownMenu/index.jsx
+++ front-end/src/components/SearchForm/DropDownMenu/index.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useRef, useState } from "react";
+import useClickDetect from "./useClickDetect";
 import "./DropDownMenu.scss";
+
 function DropDownMenu({ svgIcon, hint, id }) {
+  //States
+  const [focused, setFocused] = useState(false);
+
+  //Refs
+  const dropDownBoxRef = useRef();
+
+  //Effects
+  // useEffect(() => {
+  //   /*Called whenever setFocused is called*/
+  //   dropDownBoxRef.current.querySelector(".drop-down-input").focus();
+  // }, [focused]);
+  // useEffect(() => {
+  //   function handleClickOutside(event) {
+  //     if (
+  //       dropDownBoxRef.current &&
+  //       !dropDownBoxRef.current.contains(event.target)
+  //     ) {
+  //       setFocused(false);
+  //     }
+  //   }
+  //   // Bind the event listener
+  //   document.addEventListener("mousedown", handleClickOutside);
+  //   return () => {
+  //     // Unbind the event listener on clean up
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   };
+  // }, [dropDownBoxRef]);
+  //Functions
+  // const requestFocus = () => {
+  //   // dropDownBoxRef.current.setFocused(true);
+  //   setFocused(true);
+  // };
+  //Functions
+  const requestFocus = () => {};
   return (
-    <div className="drop-down-box">
+    <div
+      onClick={() => {
+        setFocused(true);
+      }}
+      className={`drop-down-box ${focused ? "focused" : ""}`}
+      id={id}
+      ref={dropDownBoxRef}
+    >
       {svgIcon}
       <input type="text" placeholder={hint} className="drop-down-input" />
     </div>
--- front-end/src/components/SearchForm/index.jsx
+++ front-end/src/components/SearchForm/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import DropDownMenu from "./DropDownMenu";
 import IconButton from "./IconButton";
 import { ReactComponent as SearchIcon } from "../../assets/icon_search.svg";
-import { ReactComponent as SpecialyIcon } from "../../assets/icon_specialty.svg";
+import { ReactComponent as SpecialtyIcon } from "../../assets/icon_specialty.svg";
 import { ReactComponent as DoctorIcon } from "../../assets/icon_doctor.svg";
 import "./SearchForm.scss";
 function SearchForm() {
   return (
-    <form id="search-form">
-      <DropDownMenu svgIcon={<SpecialyIcon />} hint={"Specialy"} />
-      <DropDownMenu svgIcon={<DoctorIcon />} hint={"Doctor Name (optional"} />
-      <IconButton svgIcon={<SearchIcon />} id="search-button" />
-    </form>
+    // <form id="search-form">
+    <DropDownMenu svgIcon={<SpecialtyIcon />} hint={"Specialty"} />
+    // <DropDownMenu svgIcon={<DoctorIcon />} hint={"Doctor Name (optional"} />
+    // <IconButton svgIcon={<SearchIcon />} id="search-button" />
+    // </form>
   );
 }
 export default SearchForm;
