bdb0a226725493d6a66bd1939d213f4af3c6b4fd
Ali Mhammad <alimhmd.engineer@gmail.com>
2022-05-05 18:10:12 +0300

    Working on session

--- back-end/server.js
+++ back-end/server.js
@@ -5,14 +5,27 @@ const doctorRouter = require("./routes/doctor.js");
 const specialtyRouter = require("./routes/specialty.js");
 const patientRouter = require("./routes/patient");
 const AccountRouter = require("./routes/account");
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+require("dotenv").config()
 /* This module is responsible for creating and manipulating
 our server */
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: "Test Secret",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.YOUR_DOCTOR_DB_URI,
+    }),
+    cookie:{
+        maxAge: 1000*60*60*24,
+    }
+  }))
 //Routes and APIs
 // app.use("/test", test);
 app.use("/basicUser", basicUserRouter);
