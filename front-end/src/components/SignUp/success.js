64837a60e83fd26ed0417794ae0b6c80195b9c0c
MarwanMcheik <93215789+MarwanMcheik@users.noreply.github.com>
2022-05-05 03:16:24 +0300

    Snackbar Added to SignUp

---
+++ front-end/src/components/SignUp/success.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import "./SignUp.css";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function CustomizedSnackbars() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar
+      open={open}
+      className="snack"
+      autoHideDuration={4000}
+      onClose={handleClose}
+    >
+      <Alert
+        onClose={handleClose}
+        className="alert"
+        severity="success"
+        sx={{ width: "120%" }}
+      >
+        Account Succefully Created!
+      </Alert>
+    </Snackbar>
+  );
+}
